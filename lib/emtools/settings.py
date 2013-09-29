75f7547a72bae2f0c7d5bcc662f440c9989d784c
Jorgen Schaefer <forcer@forcix.cx>
2013-09-29 17:48:38 +0200

    Re-add template dir, apparently needed.

--- lib/emtools/settings.py
+++ lib/emtools/settings.py
@@ -66,6 +66,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    "/srv/emcom/electusmatari.com/lib/emtools/templates/"
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
