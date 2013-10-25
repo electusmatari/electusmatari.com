464c5353b02ea82cb2b1806c9588276fdc14f2d3
Jorgen Schaefer <contact@jorgenschaefer.de>
2013-10-25 11:52:52 +0200

    cleanapicache: Close DB connection explicitly
    For some reason, this wasn't closed at the end of the command, causing
    a log entry every 10 minutes.

--- lib/emtools/ccpeve/management/commands/cleanapicache.py
+++ lib/emtools/ccpeve/management/commands/cleanapicache.py
@@ -1,10 +1,13 @@
 import datetime
 
 from django.core.management.base import BaseCommand
+import django.db
 
 from emtools.ccpeve.models import Cache
+
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         Cache.objects.filter(cacheduntil__lt=datetime.datetime.utcnow()
                              ).delete()
+        django.db.close_connection()
